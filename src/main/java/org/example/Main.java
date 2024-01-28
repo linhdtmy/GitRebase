eaf49420aa3fe1b4efb9b457e9694c8b4abe67e4
dangthimylinh <linhboure@gmail.com>
2024-01-28 16:14:19 +0700

    Create project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my day!");
+        System.out.println("Today is Fun ");
     }
 }
